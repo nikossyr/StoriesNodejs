ee654bbd98382d5dae45af6ba32988e01fbd6394
nikossyr <b_eliever@hotmail.com>
2020-08-25 14:51:07 +0300

    Created routes, basic templates and views

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db');
 
 // Load config
@@ -11,9 +13,18 @@ connectDB();
 
 const app = express();
 
+// Logging
 if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'));
 }
+
+// Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+// @desc    Login/Landing page
+// @route   GET /
+router.get("/", (req, res) => {
+    res.render('login');
+})
+
+// @desc    Dashboard
+// @route   GET /dashboard
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+})
+
+module.exports = router;
