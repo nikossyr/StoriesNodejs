583f202a9943fa656b3d54470735c0b36fd2eab6
nikossyr <b_eliever@hotmail.com>
2020-08-25 16:55:58 +0300

    Implemented Google Oauth2 authentication. After authentication the profile data is logged in the console and the website just hangs and does not redirect yet.

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -20,11 +25,24 @@ if (process.env.NODE_ENV === "development") {
 
 // Handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.set('view engine', '.hbs')
+
+// Session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session())
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
