e9b808cb76380ff4452e7f0545c0045f401285c7
nikossyr <b_eliever@hotmail.com>
2020-08-25 14:13:36 +0300

    Connected MongoDB to app

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({path: './config/config.env'})
 
+connectDB();
+
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan('dev'));
+}
 
 const PORT = process.env.PORT || 5000;
 
