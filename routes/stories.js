df30021e7c64feb68699beb58927d63f9446eb28
nikossyr <b_eliever@hotmail.com>
2020-09-09 19:22:59 +0300

    Implemented show single story page.

--- routes/stories.js
+++ routes/stories.js
@@ -41,6 +41,25 @@ router.get("/", ensureAuth, async (req, res) => {
     }
 });
 
+// @desc    Show single story page
+// @route   GET /stories/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id)
+            .populate('user')
+            .lean();
+
+        if (!story) return res.render('errors/404');
+
+        res.render('stories/show', {
+            story
+        });
+    } catch (err) {
+        console.error(err);
+        res.render('errors/404');
+    }
+});
+
 // @desc    Show edit page
 // @route   GET /stories/edit/:id
 router.get("/edit/:id", ensureAuth, async (req, res) => {
